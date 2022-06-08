e47881f42fd54034c2a881a7c5379b923c3bd3ff
sane cheikh oumar <safietou2717@gmail.com>
2022-06-08 17:09:06 +0200

    index

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+class voiture {
+    public $couleur;
+    public $masse;
+    public $marque;
+    public $vitesse=0;
+
+    public function afficherMessage()= float
+    {
+        echo "je suis une voiture";    }
+
+}
+$voiture1 = new voiture():float;
+$voiture1 -> couleur= "vert";
+?>
